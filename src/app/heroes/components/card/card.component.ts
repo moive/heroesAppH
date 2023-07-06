3d5a6aabcf9958f7faf44bf297a4840f7588f0cc
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-07-05 22:18:41 -0500

    list heroes with card images and pipe heroImage

---
+++ src/app/heroes/components/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+
+@Component({
+  selector: 'heroes-hero-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+})
+export class CardComponent implements OnInit {
+  @Input() hero!: Hero;
+
+  ngOnInit(): void {
+    if (!this.hero) throw Error('Hero property is required');
+  }
+}
